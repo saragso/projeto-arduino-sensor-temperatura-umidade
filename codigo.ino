451f2a32a717c6647c4d8b4d9238c17dc96d889d
Sara Oliveira <138247567+saragso@users.noreply.github.com>
2023-10-24 17:38:09 -0300

    atualiza o código do projeto

--- codigo.ino
+++ codigo.ino
@@ -1,49 +1,21 @@
-#include "DHT.h"
+#include <dht.h> //inlcui a biblioteca dht
 
-#define pinoA2 A2 //define qual pino do Arduino será usado.
+const int pinoDHT11 = A2; //pino analógios utilizado pelo DHT11
 
-#define DHTTYPE DHT11 //define qual tipo de DHT será usado (11, 21 ou 22)
-
-//Inicia o sensor DHT
-DHT dht(pinoA2, DHTTYPE); 
+dht DHT; //variável do tipo DHT
 
 void setup(){
-  Serial.begin(9600); //Inicializa a Serial
-  delay(2000); //Intervalo de 2 segundos antes de começar
-  Serial.println(F("Sistema inicializado!"));
-  dht.begin();
+  Serial.begin(9600); //inicializa a serial
+  delay(2000); //intervalo de 2 segundos antes de iniciar
 }
 
 void loop(){
-  delay(2000);
-
-  //Definição das variáveis de humidade e temperatura.
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-  // Se necessário, inclua essa variável (lê temperatura em Fahrenheit)
-  //float f = dht.readTemperature(true);
-
-  //Verifica se houve erros e tenta novamente.
-  if (isnan(h) || isnan(t)) {
-    Serial.println(F("Houve um erro na leitura do sensor!"));
-    return;
-  }
-
-  /* Inclua se quiser adicionar Fahrenheit, remova o de cima
-   if (isnan(h) || isnan(t)) || isnan(f) {
-    Serial.println(F("Houve um erro na leitura do sensor!"));
-    return;
-  }
-  */
-
-  //Imprime o texto na Serial
-  Serial.print("Umidade: "); 
-  Serial.print(h);
-  Serial.print("%"); 
-
-  Serial.print(" | Temperatura: ");
-  Serial.print(t);
-  Serial.println("°C \n");
-  delay(2000); //Intervalo de 2 segundos
+  DHT.read11(pinoDHT11); //lé as informações do sensor
+  Serial.print("Umidade: "); //imprime o texto na serial
+  Serial.print(DHT.humidity); //imprime na serial o valor de umidade medido
+  Serial.print("%"); //escreve o texto em seguida
+  Serial.print(" / Temperatura: "); //imprime o texto na serial
+  Serial.print(DHT.temperature, 0); //imprime na serial o valor de umidade medido e remove a parte decimal
+  Serial.println("*C"); //imprime o texto na serial
+  delay(2000); //intervalo de 2 segundos (não diminuir esse valor)
 }
